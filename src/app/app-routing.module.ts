048105c9fdd84b7c893536383683906099ffa0d1
Dannemaniman <daniellangas91@gmail.com>
2022-04-11 01:21:02 +0200

    Created basic styling for User Profile Page.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router'
 import { CategoryPageComponent } from "./modules/category-page/category-page.component"
 import { HomePageComponent } from "./modules/home-page/home-page.component"
 import { ThreadDetailPageComponent } from "./modules/thread-detail-page/thread-detail-page.component"
+import { UserProfilePageComponent } from "./modules/user-profile-page/user-profile-page.component"
 
 const routes: Routes = [
   { path: "", component: HomePageComponent, pathMatch: 'full' },
   { path: "category/:id", component: CategoryPageComponent },
   { path: "category/:id/thread/:id", component: ThreadDetailPageComponent },
+  { path: "user-page", component: UserProfilePageComponent },
 ]
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
-import { CoreModule } from './core/core.module';
-import { HomePageComponent } from './modules/home-page/home-page.component';
-import { CategoryPageComponent } from './modules/category-page/category-page.component';
+import { CoreModule } from './core/core.module'
+import { HomePageComponent } from './modules/home-page/home-page.component'
+import { CategoryPageComponent } from './modules/category-page/category-page.component'
 import { ThreadDetailPageComponent } from './modules/thread-detail-page/thread-detail-page.component'
+import { UserProfilePageComponent } from './modules/user-profile-page/user-profile-page.component'
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { ThreadDetailPageComponent } from './modules/thread-detail-page/thread-d
     HomePageComponent,
     CategoryPageComponent,
     ThreadDetailPageComponent,
+    UserProfilePageComponent,
   ],
   imports: [
     BrowserModule,
